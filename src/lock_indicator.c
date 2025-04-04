0fc02919fa17518e74c19db8f129739426f5d922
TBog <thetbog@gmail.com>
2025-04-04 22:06:17 +0300

    change multiple instances to multiple children

--- src/lock_indicator.c
+++ src/lock_indicator.c
@@ -36,23 +36,23 @@ DT_INST_FOREACH_CHILD(0, LI_STRUCT)
 #define LOCK_INDICATOR_DATA_REF_AND_COMMA(n) \
     &lock_indicator_config_##n,
 
-static struct lock_indicator_config *lock_indicator_instances[] = {
+static struct lock_indicator_config *lock_indicator_child[] = {
     DT_INST_FOREACH_CHILD(0, LOCK_INDICATOR_DATA_REF_AND_COMMA)
 };
 
 static zmk_hid_indicators_t zmk_event_indicator_previous = {0};
 
-#define LOCK_INDICATOR_INSTANCE_COUNT ARRAY_SIZE(lock_indicator_instances)
+#define LOCK_INDICATOR_CHILD_COUNT ARRAY_SIZE(lock_indicator_child)
 
 static int lock_indicator_listener(const zmk_event_t *eh) {
     const struct zmk_hid_indicators_changed *ev = as_zmk_hid_indicators_changed(eh);
     const zmk_hid_indicators_t indicators = ev->indicators;
-    const size_t num_indicators = LOCK_INDICATOR_INSTANCE_COUNT;
+    const size_t num_indicators = LOCK_INDICATOR_CHILD_COUNT;
 
     LOG_INF("lock_indicator_listener indicators=%#04X count=%d", indicators, num_indicators);
     // Iterate over all instances
     for (size_t i = 0; i < num_indicators; i+=1) {
-        const struct lock_indicator_config *data = lock_indicator_instances[i];
+        const struct lock_indicator_config *data = lock_indicator_child[i];
         const bool old_led_state = (zmk_event_indicator_previous & data->indicator_mask) != 0;
         const bool new_led_state = (indicators & data->indicator_mask) != 0;
         if (old_led_state != new_led_state) {
@@ -70,9 +70,10 @@ static int lock_indicator_listener(const zmk_event_t *eh) {
 
 static int sys_lock_indicator_init() {
     size_t count = 0;
+    const size_t num_indicators = LOCK_INDICATOR_CHILD_COUNT;
     // Iterate over all instances
-    for (size_t i = 0; i < LOCK_INDICATOR_INSTANCE_COUNT; i+=1) {
-        const struct lock_indicator_config *data = lock_indicator_instances[i];
+    for (size_t i = 0; i < num_indicators; i+=1) {
+        const struct lock_indicator_config *data = lock_indicator_child[i];
     
         if (!device_is_ready(data->led_gpio.port)) {
             LOG_WRN("Lock Indicator GPIO device not ready\n");
@@ -87,7 +88,7 @@ static int sys_lock_indicator_init() {
         gpio_pin_set_dt(&data->led_gpio, 0);
         count += 1;
     }
-    LOG_INF("lock-indicator initialized %d/%d", count, LOCK_INDICATOR_INSTANCE_COUNT);
+    LOG_INF("lock-indicator initialized %d/%d", count, num_indicators);
     return 0;
 }
 
